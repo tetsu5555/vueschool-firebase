5a19557176323769b29cad31001ea4037925909c
tetsu5555 <tetete1118@gmail.com>
2019-05-14 04:08:17 +0900

    pudate index.js to return index.html

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("hoge");
-})
+    var url = "public" + req.url;
+    if (fs.existsSync(url)) {
+        fs.readFile(url, (err, data) => {
+            if (!err) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            }
+        });
+    }
+});
 
 var port = process.env.PORT || 3001
 server.listen(port, function () {
